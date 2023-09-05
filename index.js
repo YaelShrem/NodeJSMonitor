73b7ede66fa23f03d6e5059ef11da0e82e080974
YaelShrem <y0583239076@gmail.com>
2023-09-05 12:18:22 +0300

    basic setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const responseTime=require("response-time");
+const logger=require("./utils/logger");
+const swaggerDocs=require("./utils/swagger");
+const { restResponseTimeHistogram, startMetricsServer }=require("./utils/metrics");
+
+app.use(express.json());
+app.use(
+    responseTime((req, res, time) => {
+      if (req?.route?.path) {
+        restResponseTimeHistogram.observe(
+          {
+            method: req.method,
+            route: req.route.path,
+            status_code: res.statusCode,
+          },
+          time * 1000
+        );
+      }
+    })
+  );
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(PORT, async () => {
+    logger.info(`App is running at http://localhost:${port}`);
+    startMetricsServer();
+    swaggerDocs(app, port);
+});
+
---
+++ utils/swagger.js
@@ -0,0 +1,46 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const { version } = require("../../package.json");
+const log = require("./logger");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "REST API Docs",
+      version,
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes.js", "./src/schema/*.js"], // Note: updated file extensions to .js
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+function swaggerDocs(app, port) {
+  // Swagger page
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  // Docs in JSON format
+  app.get("/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+
+  log.info(`Docs available at http://localhost:${port}/docs`);
+}
+
+module.exports = swaggerDocs;
